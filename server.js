6fede7d15e5232785d76a45f316f5da257161f40
MuhammadGhayas2 <unknownartist547@gmail.com>
2026-08-19 03:25:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import http from 'node:http'
+import {getDataFromDB} from './db.js'
 const PORT=8000
 
-const server=http.createServer((req,res)=>{
-    res.end("Hello from server")
+const server=http.createServer(async(req,res)=>{
+    const urlObj=new URL(req.url,`http://${req.headers.host}`)
+    const data=await getDataFromDB() 
+    if(urlObj.pathname==='/api/questions'){
+        res.end(JSON.stringify(data))
+    }
+
 })
+
 
 server.listen(PORT,()=>console.log(`Server is Listening at PORT ${PORT}`))
